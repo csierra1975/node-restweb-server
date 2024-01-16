f30278c98e646dc16828cbf4500919999eab2b5d
csierra1975 <carlos.sierra.chacon@gmail.com>
2024-01-16 11:22:39 +0100

    Update rest web api

---
+++ src/presentation/todos/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express"
+import { TodosController } from "./controller"
+
+export class TodosRoutes {
+
+    static get routes(): Router {
+
+        const router = Router()
+        const todoController = new TodosController()
+
+        router.get('/', todoController.getTodos)
+        router.get('/:id', todoController.getTodoById)
+        router.post('/', todoController.createTodo)
+        router.put('/:id', todoController.updateTodo)
+        router.delete('/:id', todoController.deleteTodo)
+        //router.get('/api/todos', (req, res) => todoController.getTodos(req, res))
+        
+        return router
+    }
+}
